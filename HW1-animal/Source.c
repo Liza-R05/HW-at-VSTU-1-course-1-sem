79583ce96bc7288e8f89a04065040d087cd28d2d
Elizaveta Rassokha <lenta_sv@mail.ru>
2025-09-11 18:08:56 +0300

    Add files via upload

---
+++ HW1-animal/Source.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+	puts("      scorpion      ");
+	puts("                    ");
+	puts("       _    _       ");
+	puts("      /_]  [_ |     ");
+	puts("     //      ||     ");
+	puts("     ||      //     ");
+	puts("      |`-..-'/      ");
+	puts("       `|  /`       ");
+	puts("         ||    _    ");
+	puts("         ||   ( )   ");
+	puts("         ||___/     ");
+	puts("                    ");
+	puts("                    ");
+}
