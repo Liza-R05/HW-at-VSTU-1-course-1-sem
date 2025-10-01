4f63308e933f31fe3ea4877feae8fc642ad1d50b
Elizaveta Rassokha <lenta_sv@mail.ru>
2025-10-01 17:29:11 +0300

    Update main.c

--- HW-04/main.c
+++ HW-04/main.c
@@ -8,14 +8,14 @@ void main()
 {
 	setlocale(LC_ALL, "RUS");
 	float x, y;
-	printf("Введите значение х: ");
+	printf("Р’РІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёРµ С…: ");
 	scanf("%f", &x);
 	printf("\nx=%.1f", x);
-	printf("\nВведите значение y: ");
+	printf("\nР’РІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёРµ y: ");
 	scanf("%f", &y);
 	printf("\ny=%.1f", y);
 	printf("\n=======================================\n");
-	printf("\nРассчитаем значение F\n");
+	printf("\nР Р°СЃСЃС‡РёС‚Р°РµРј Р·РЅР°С‡РµРЅРёРµ F\n");
 	printf("F=%f\n", (2.33 * log(sqrt(1+pow(cos(x),2))))/(exp(y)+pow(sin(x),2)));
 	getchar();
 }
