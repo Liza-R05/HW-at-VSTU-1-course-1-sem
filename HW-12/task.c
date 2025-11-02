42385bba4a5025dad1a5f4ca87ad662b1767a7aa
Elizaveta Rassokha <lenta_sv@mail.ru>
2025-11-02 16:19:06 +0300

    Update task.c

--- HW-12/task.c
+++ HW-12/task.c
@@ -18,19 +18,19 @@ void main()
     double arr_double[2];
     signed int arr_sint[2];
 
-    // вычисляем размеры через разницу адресов элементов массива, тк в массивах элементы идут последовательно и
-    // разница адресов соседних элементов равна размеру типа данных в байтах
+    // РІС‹С‡РёСЃР»СЏРµРј СЂР°Р·РјРµСЂС‹ С‡РµСЂРµР· СЂР°Р·РЅРёС†Сѓ Р°РґСЂРµСЃРѕРІ СЌР»РµРјРµРЅС‚РѕРІ РјР°СЃСЃРёРІР°, С‚Рє РІ РјР°СЃСЃРёРІР°С… СЌР»РµРјРµРЅС‚С‹ РёРґСѓС‚ РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕ Рё
+    // СЂР°Р·РЅРёС†Р° Р°РґСЂРµСЃРѕРІ СЃРѕСЃРµРґРЅРёС… СЌР»РµРјРµРЅС‚РѕРІ СЂР°РІРЅР° СЂР°Р·РјРµСЂСѓ С‚РёРїР° РґР°РЅРЅС‹С… РІ Р±Р°Р№С‚Р°С…
 
-    printf("1. char:               %ld байт\n", (long)((char*)&arr_char[1] - (char*)&arr_char[0]));
-    printf("2. short int:          %ld байт\n", (long)((char*)&arr_short[1] - (char*)&arr_short[0]));
-    printf("3. unsigned short int: %ld байт\n", (long)((char*)&arr_ushort[1] - (char*)&arr_ushort[0]));
-    printf("4. int:                %ld байт\n", (long)((char*)&arr_int[1] - (char*)&arr_int[0]));
-    printf("5. unsigned char:      %ld байт\n", (long)((char*)&arr_uchar[1] - (char*)&arr_uchar[0]));
-    printf("6. wchar_t:            %ld байт\n", (long)((char*)&arr_wchar[1] - (char*)&arr_wchar[0]));
-    printf("7. long long int:      %ld байт\n", (long)((char*)&arr_llint[1] - (char*)&arr_llint[0]));
-    printf("8. size_t:             %ld байт\n", (long)((char*)&arr_size_t[1] - (char*)&arr_size_t[0]));
-    printf("9. float:              %ld байт\n", (long)((char*)&arr_float[1] - (char*)&arr_float[0]));
-    printf("10. double:            %ld байт\n", (long)((char*)&arr_double[1] - (char*)&arr_double[0]));
-    printf("11. signed int:        %ld байт\n", (long)((char*)&arr_sint[1] - (char*)&arr_sint[0]));
+    printf("1. char:               %ld Р±Р°Р№С‚\n", (long)((char*)&arr_char[1] - (char*)&arr_char[0]));
+    printf("2. short int:          %ld Р±Р°Р№С‚\n", (long)((char*)&arr_short[1] - (char*)&arr_short[0]));
+    printf("3. unsigned short int: %ld Р±Р°Р№С‚\n", (long)((char*)&arr_ushort[1] - (char*)&arr_ushort[0]));
+    printf("4. int:                %ld Р±Р°Р№С‚\n", (long)((char*)&arr_int[1] - (char*)&arr_int[0]));
+    printf("5. unsigned char:      %ld Р±Р°Р№С‚\n", (long)((char*)&arr_uchar[1] - (char*)&arr_uchar[0]));
+    printf("6. wchar_t:            %ld Р±Р°Р№С‚\n", (long)((char*)&arr_wchar[1] - (char*)&arr_wchar[0]));
+    printf("7. long long int:      %ld Р±Р°Р№С‚\n", (long)((char*)&arr_llint[1] - (char*)&arr_llint[0]));
+    printf("8. size_t:             %ld Р±Р°Р№С‚\n", (long)((char*)&arr_size_t[1] - (char*)&arr_size_t[0]));
+    printf("9. float:              %ld Р±Р°Р№С‚\n", (long)((char*)&arr_float[1] - (char*)&arr_float[0]));
+    printf("10. double:            %ld Р±Р°Р№С‚\n", (long)((char*)&arr_double[1] - (char*)&arr_double[0]));
+    printf("11. signed int:        %ld Р±Р°Р№С‚\n", (long)((char*)&arr_sint[1] - (char*)&arr_sint[0]));
     system("pause");
 }
