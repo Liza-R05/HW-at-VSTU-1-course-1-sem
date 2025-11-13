0e0d69094f90a36e37e648ed1710bcc42aaf0437
Elizaveta Rassokha <lenta_sv@mail.ru>
2025-11-13 19:01:05 +0300

    Update task.c

--- HW-25/task.c
+++ HW-25/task.c
@@ -4,72 +4,71 @@
 #include <locale.h>
 #include <time.h>
 
-// заполнение массива случайными числами от -1 до 1
-double* full_elements(double* ptr_array, int n) 
+// заполнение массива случайными числами от 1 до K
+int* full_elements(int* ptr_array, int n, int K)
 {
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
     {
-        ptr_array[i] = -1.0 + 2.0 * rand() / RAND_MAX;
+        ptr_array[i] = 1 + rand() % K;
     }
     return ptr_array;
 }
 
-// печать элементов массива
-int put_elements(double* ptr_array, int n) 
+// печать массива
+int put_elements(int* ptr_array, int n)
 {
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
     {
-        printf("[%d] = %.2f\n", i, ptr_array[i]);
+        printf("[%d] = %d\n", i, ptr_array[i]);
     }
     return n;
 }
 
-// реверс элементов массива
-double* reverse_array(double* start, double* end, int* new_size) 
+// реверс элементов
+int* reverse_array(int* start, int* end, int* new_size)
 {
     int size = end - start + 1;
     *new_size = size;
-    double* revers = (double*)malloc(size * sizeof(double));
-    if (revers == NULL) 
+    int* reversed = (int*)malloc(size * sizeof(int));
+    if (reversed == NULL)
     {
         printf("error\n");
         return NULL;
     }
-
-    for (int i = 0; i < size; i++) 
+    for (int i = 0; i < size; i++)
     {
-        revers[i] = start[size - 1 - i];
+        reversed[i] = start[size - 1 - i];
     }
-    return revers;
+    return reversed;
 }
 
-void main() 
+void main()
 {
     setlocale(LC_ALL, "RUS");
     srand(time(NULL));
-    int n = 10 + rand() % 41;
-    // заполнение массива, выд. память
-    double* a = (double*)malloc(n * sizeof(double));
-    if (a == NULL) 
+    int K, N;
+    printf("Введите K (максимальное значение): ");
+    scanf("%d", &K);
+    printf("Введите N (количество элементов): ");
+    scanf("%d", &N);
+    int* array = (int*)malloc(N * sizeof(int));
+    if (array == NULL)
     {
         printf("error\n");
         return -1;
     }
-    full_elements(a, n);
-    // реверс. массива
+    full_elements(array, N, K);
+    printf("\nИсходный массив (%d элементов):\n", N);
+    put_elements(array, N);
+    // реверс массива
     int new_size;
-    double* revers = reverse_array(&a[0], &a[n - 1], &new_size);
-    if (revers == NULL) 
+    int* reversed_array = reverse_array(&array[0], &array[N - 1], &new_size);
+    if (reversed_array != NULL)
     {
-        printf("error\n");
-        free(a);
-        return -1;
+        printf("\nРеверсированный массив (%d элементов):\n", new_size);
+        put_elements(reversed_array, new_size);
+        free(reversed_array);
     }
-    printf("Исходный массив (%d элементов):\n", n);
-    put_elements(a, n);
-    printf("\nРеверсированный массив (%d элементов):\n", new_size);
-    put_elements(revers, new_size);
-    free(a);
-    free(revers);
+    free(array);
     system("pause");
 }
